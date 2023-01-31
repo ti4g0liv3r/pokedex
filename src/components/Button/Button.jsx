5b5fef4de89fcb4c66b01a8564fa5cee7ef65114
Tiago Pinheiro <tiagoliver@pm.me>
2023-01-31 15:16:19 +0100

    Added new Link Button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,32 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { FaArrowLeft } from "react-icons/fa";
 
 const StyledButton = styled.button``;
 
 export const Button = ({ type = "primary", name = "button" }) => {
   return <StyledButton type={type}>{name}</StyledButton>;
+};
+
+const ButtonLinkWrapper = styled.div`
+  width: 40px;
+  height: 40px;
+  background-color: #ffffff5e;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+
+  > a {
+    color: white;
+    padding-top: 3px;
+  }
+`;
+
+export const BackButton = ({ path = "", text }) => {
+  return (
+    <ButtonLinkWrapper>
+      <Link to={path}>{text ? text : <FaArrowLeft />}</Link>
+    </ButtonLinkWrapper>
+  );
 };
