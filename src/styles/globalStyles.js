2a48014585f9edb90005c3f680f1b446ab6ec741
Tiago Pinheiro <tiagoliver@pm.me>
2023-01-31 04:30:28 +0100

    Added default global styles

---
+++ src/styles/globalStyles.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: auto;
+    padding: 0;
+      background-color: #f7f7f7;
+    font-family: Open-Sans, Helvetica, Sans-Serif;
+  }
+`;
+
+export default GlobalStyle;
