408b3829e68a1440494a1567a84652c52f197483
Tiago Pinheiro <tiagoliver@pm.me>
2023-01-31 18:00:17 +0100

    Added search input to home

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,8 +1,45 @@
+import { useState } from "react";
+import styled from "styled-components";
 import { PokemonList, Title } from "../../components";
 import { usePokeProvider } from "../../hooks";
 
+const SearchContainer = styled.div`
+  width: 100%;
+  text-align: center;
+`;
+
+const SearchInput = styled.input`
+  border: 1px solid lightgray;
+  height: 25px;
+  padding: 10px;
+  border-radius: 20px;
+  min-width: 300px;
+`;
+
+const SearchBar = ({ search, setSearch }) => {
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+  return (
+    <SearchContainer>
+      <SearchInput
+        type="text"
+        value={search}
+        onChange={(e) => handleChange(e)}
+        placeholder="Search for a pokemon"
+      />
+    </SearchContainer>
+  );
+};
+
 export const Home = () => {
   const { data, isLoading, error } = usePokeProvider();
+  const [search, setSearch] = useState("");
+
+  const filteredResults = data.filter((pokemon) =>
+    pokemon.name.includes(search.toLowerCase())
+  );
+  const listToShow = search.length === 0 ? data : filteredResults;
 
   if (isLoading) return <Title size={1}>Cargando...</Title>;
 
@@ -12,7 +49,8 @@ export const Home = () => {
     );
   return (
     <>
-      <PokemonList list={data} />;
+      <SearchBar search={search} setSearch={setSearch} />
+      <PokemonList list={listToShow} />
     </>
   );
 };
