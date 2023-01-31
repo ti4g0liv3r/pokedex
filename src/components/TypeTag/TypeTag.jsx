4cb70ce087f667d155ae00a3ff4ce594a202fc47
Tiago Pinheiro <tiagoliver@pm.me>
2023-01-31 17:10:02 +0100

    Adjusted tag color

--- src/components/TypeTag/TypeTag.jsx
+++ src/components/TypeTag/TypeTag.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const StyledTag = styled.small`
   padding: 5px 10px;
-  background-color: #ffffff5c;
+  background-color: #6666665c;
   color: white;
   margin: 0px 5px;
   border-radius: 50px;
