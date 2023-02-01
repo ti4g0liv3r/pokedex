e5774986935e8400f9d6838f4b7e3843ef122b15
Tiago Pinheiro <tiagoliver@pm.me>
2023-02-01 13:40:04 +0100

    Moved logic to provider

--- src/context/pokeProvider/pokeProvider.jsx
+++ src/context/pokeProvider/pokeProvider.jsx
@@ -5,6 +5,8 @@ export const PokeContext = createContext({});
 
 export const PokeProvider = ({ children }) => {
   const { data, isLoading, error } = useClient("pokemon?limit=151");
+  const [search, setSearch] = useState("");
+  const [display, setDisplay] = useState("all");
   const [pokemon, setPokemon] = useState([]);
 
   useEffect(() => {
@@ -20,9 +22,30 @@ export const PokeProvider = ({ children }) => {
     }
   }, [isLoading, data]);
 
+  const filteredResults = pokemon.filter((pokemon) =>
+    pokemon.name.includes(search.toLowerCase())
+  );
+
+  const favPokemons = pokemon.filter((pokemon) => pokemon.isFav);
+
+  const listToShow =
+    search.length !== 0
+      ? filteredResults
+      : display === "all"
+      ? pokemon
+      : favPokemons;
+
   return (
     <PokeContext.Provider
-      value={{ data: pokemon, setPokemon, isLoading, error }}
+      value={{
+        setPokemon,
+        setSearch,
+        setDisplay,
+        display,
+        data: listToShow,
+        isLoading,
+        error,
+      }}
     >
       {isLoading ? "loading" : children}
     </PokeContext.Provider>
--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,45 +1,82 @@
-import { useState } from "react";
 import styled from "styled-components";
 import { PokemonList, Title } from "../../components";
 import { usePokeProvider } from "../../hooks";
 
-const SearchContainer = styled.div`
+const InputContainer = styled.div`
   width: 100%;
   text-align: center;
 `;
 
 const SearchInput = styled.input`
   border: 1px solid lightgray;
-  height: 25px;
+  height: 20px;
   padding: 10px;
   border-radius: 20px;
   min-width: 300px;
 `;
 
-const SearchBar = ({ search, setSearch }) => {
+const SearchBar = () => {
+  const { search, setSearch } = usePokeProvider();
   const handleChange = (e) => {
     setSearch(e.target.value);
   };
   return (
-    <SearchContainer>
+    <InputContainer>
       <SearchInput
         type="text"
         value={search}
         onChange={(e) => handleChange(e)}
         placeholder="Search for a pokemon"
       />
-    </SearchContainer>
+    </InputContainer>
   );
 };
 
+const ToolsContainer = styled.nav`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  margin: 10px;
+
+  > a {
+    width: auto;
+    margin-right: 30px;
+    cursor: pointer;
+  }
+  > a.active {
+    font-weight: bold;
+  }
+
+  > a:hover {
+    transform: scale(1.1);
+    font-weight: bold;
+  }
+`;
+
+const PokemonMenu = () => {
+  const { display, setDisplay } = usePokeProvider();
+
+  return (
+    <ToolsContainer>
+      <a
+        className={display === "all" ? "active" : ""}
+        onClick={() => setDisplay("all")}
+      >
+        All
+      </a>
+      <a
+        className={display === "fav" ? "active" : ""}
+        onClick={() => setDisplay("fav")}
+      >
+        Favorites
+      </a>
+    </ToolsContainer>
+  );
+};
 export const Home = () => {
   const { data, isLoading, error } = usePokeProvider();
-  const [search, setSearch] = useState("");
-
-  const filteredResults = data.filter((pokemon) =>
-    pokemon.name.includes(search.toLowerCase())
-  );
-  const listToShow = search.length === 0 ? data : filteredResults;
 
   if (isLoading) return <Title size={1}>Cargando...</Title>;
 
@@ -47,10 +84,12 @@ export const Home = () => {
     return (
       <Title size={1}>Oops!, seems there's an error while loading list</Title>
     );
+
   return (
     <>
-      <SearchBar search={search} setSearch={setSearch} />
-      <PokemonList list={listToShow} />
+      <SearchBar />
+      <PokemonMenu />
+      <PokemonList list={data} />
     </>
   );
 };
